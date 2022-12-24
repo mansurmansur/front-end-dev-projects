3f2d2efea7970497e9852c1cab8f22d2f83860de
mansur mansur <mansurmansur@trentu.ca>
2022-12-23 19:43:48 -0500

    js has some erros

--- Color Changer/app.js
+++ Color Changer/app.js
@@ -1,1 +1,32 @@
 "use strict"
+
+//all the elements needed
+const mainEl = document.querySelector(".main");
+let buttonEl = document.querySelectorAll(".button");
+
+
+buttonEl.forEach(button => {
+    button.addEventListener("click", (ev)=>{
+        if (mainEl.style.backgroundColor == ev.target.style.backgroundColor) {
+            //do nothing
+        } else if(ev.target.classList.contains("green")){
+            mainEl.style.backgroundColor = ev.target.style.backgroundColor;
+            console.log("Hi")
+        } else if (ev.target.classList.contains("purple")){
+            mainEl.style.backgroundColor = ev.target.style.backgroundColor;
+            console.log("Hi")
+        } else if (ev.target.classList.contains("blue-2")){
+            mainEl.style.backgroundColor = ev.target.style.backgroundColor;
+            console.log("Hi")
+        } else if (ev.target.classList.contains("blue-3")){
+            mainEl.style.backgroundColor = ev.target.style.backgroundColor;
+            console.log("Hi")
+        } else if(ev.target.classList.contains("orange")){
+            mainEl.style.backgroundColor = ev.target.style.backgroundColor;
+            console.log("Hi")
+        } else if(ev.target.classList.contains("yellow")){
+            mainEl.style.backgroundColor = ev.target.style.backgroundColor;
+            console.log("Hi")
+        }
+    })
+});
